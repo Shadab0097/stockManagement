29df617c71b65dc98ddb849f29f6134b34e20dde
shadab khan <shadabkhan2910@gmail.com>
2025-03-20 17:55:04 +0530

    bug

--- src/components/UserProductPage.jsx
+++ src/components/UserProductPage.jsx
@@ -128,8 +128,9 @@ const UserProductPage = () => {
             <div className="p-6">
                 <h1 className="text-2xl font-bold mb-6">Products</h1>
 
+
                 {/* Product List */}
-                {products ? <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
                     {products.map((product) => (
                         <div
                             key={product._id}
@@ -152,7 +153,7 @@ const UserProductPage = () => {
                         </div>
                     ))}
                 </div>
-                    : <h1 className='font-bold text-gray-600 m-auto'> No Products</h1>}
+
 
                 {/* Sell Product Modal */}
                 {isModalOpen && selectedProduct && (
