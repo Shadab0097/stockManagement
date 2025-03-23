99d451773ea21a935b8d95661c5e1d1c401b7278
shadab khan <shadabkhan2910@gmail.com>
2025-03-23 13:39:57 +0530

    bug

--- src/components/ProductManagement.jsx
+++ src/components/ProductManagement.jsx
@@ -202,7 +202,7 @@ const ProductManagement = () => {
                       <div className="text-sm text-gray-900">{product.emptyRecieved}</div>
                     </td>
                     <td className="px-4 py-2 whitespace-nowrap">
-                      <div className="text-sm text-gray-900">${product.productPrice}</div>
+                      <div className="text-sm text-gray-900">₹{product.productPrice}</div>
                     </td>
                     <td className="px-4 py-2 whitespace-nowrap">
                       <span className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${product.stockQuantity > 40 ? 'bg-green-100 text-green-800' : 'bg-yellow-100 text-yellow-800'
